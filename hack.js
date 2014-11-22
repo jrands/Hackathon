aefcbde7fbc5574d37fcaed72f7442469b572292
John <John.Randis1@Marist.edu>
2014-11-21 20:47:58 -0500

    sends query to API

--- hack.js
+++ hack.js
@@ -6,3 +6,28 @@ var getURL = function(){
   return url;
 
 }
+
+var sendQuery = function(apiURL){
+
+  xhr = new XMLHttpRequest();
+  xhr.open('POST', apiURL, true);
+  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+  
+  xhr.onload = function () {
+    response=JSON.parse(this.responseText);
+    docs=response["documents"];
+    for (i=0;i<docs.length;i++){
+        console.log(docs[i]["title"])
+    }
+  }
+  
+  xhr.send('text=great&apikey=31073e76-4d1f-4615-9375-694ebfad9db7');
+  
+  var data = new FormData();
+  data.append('apikey', '31073e76-4d1f-4615-9375-694ebfad9db7');
+  data.append('text', 'great');
+  var test = xhr.send(data);
+  return test;
+}
+
+document.getElementById("para").innerHTML = sendQuery(getURL);
