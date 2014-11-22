b231f8b701d62d084f1c172d266d42522b767eab
John <John.Randis1@Marist.edu>
2014-11-22 18:09:29 -0500

    Can now pass in any image URL to detect

--- hack.js
+++ hack.js
@@ -1,19 +1,21 @@
-var path = encodeURIComponent("http://sugaringnyc.com/wp-content/uploads/2014/07/face.jpg");
 
-var apiURL = "http://api.idolondemand.com/1/api/sync/detectfaces/v1?url=" + path + "&apikey=31073e76-4d1f-4615-9375-694ebfad9db7";  
 
+var apiURL;
+
+var setURL = function(url){
+
+  var path = encodeURIComponent(url);
+   
+  apiURL = "http://api.idolondemand.com/1/api/sync/detectfaces/v1?url=" + path + "&apikey=31073e76-4d1f-4615-9375-694ebfad9db7";
+}
 
 var sendRequest = function(){
-
   var xhr = new XMLHttpRequest();
-
   xhr.open("GET", apiURL, false);
-  
   xhr.send();
-  
   var xhrDoc = xhr.response;
-  
   console.log(xhrDoc);
 }
 
-sendRequest();
+setURL("http://spinoff.comicbookresources.com/wp-content/uploads/2013/09/jack_black.jpg");
+
