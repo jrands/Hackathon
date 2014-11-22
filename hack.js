e56e069ec30d2fa70e787d5c4b1f62505970b049
John <John.Randis1@Marist.edu>
2014-11-22 01:55:20 -0500

    edited query requestor, still needs work

--- hack.js
+++ hack.js
@@ -1,33 +1,24 @@
 
-var getURL = function(){
 
-  var url = "/1/api/sync/detectfaces/v1 file=my-face.jpg";
+var apiURL = "http://api.idolondemand.com/1/api/sync/detectfaces/v1 file=my-face.jpg";
   
-  return url;
 
-}
+var xhr = new XMLHttpRequest();
+xhr.open('POST', apiURL, true);
 
-var sendQuery = function(apiURL){
-
-  xhr = new XMLHttpRequest();
-  xhr.open('POST', apiURL, true);
-  xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-  
-  xhr.onload = function () {
+xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+xhr.onload = function () {
+    var response = "";
     response=JSON.parse(this.responseText);
     docs=response["documents"];
     for (i=0;i<docs.length;i++){
-        console.log(docs[i]["title"])
+        alert(docs[i]["title"]);
     }
-  }
-  
-  xhr.send('text=great&apikey=31073e76-4d1f-4615-9375-694ebfad9db7');
-  
-  var data = new FormData();
-  data.append('apikey', '31073e76-4d1f-4615-9375-694ebfad9db7');
-  data.append('text', 'great');
-  var test = xhr.send(data);
-  return test;
-}
+};
 
-document.getElementById("para").innerHTML = sendQuery(getURL);
+xhr.send('text=great&apikey=31073e76-4d1f-4615-9375-694ebfad9db7');
+
+var data = new FormData();
+data.append('apikey', '31073e76-4d1f-4615-9375-694ebfad9db7');
+data.append('text', 'great');
+xhr.send(data);
