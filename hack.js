cc400ba96af0bb7df9d05c4b24059ced2ae5d392
John <John.Randis1@Marist.edu>
2014-11-22 17:36:25 -0500

    Sends request with encoded URI, no longer redirects

--- hack.js
+++ hack.js
@@ -1,19 +1,19 @@
+var path = encodeURIComponent("http://sugaringnyc.com/wp-content/uploads/2014/07/face.jpg");
 
-var apiURL = "http://api.idolondemand.com/1/api/sync/detectfaces/v1";  
+var apiURL = "http://api.idolondemand.com/1/api/sync/detectfaces/v1?url=" + path + "&apikey=31073e76-4d1f-4615-9375-694ebfad9db7";  
 
 
 var sendRequest = function(){
+
   var xhr = new XMLHttpRequest();
 
-  xhr.onload = reqListener(xhr);
-
-  xhr.open("POST", apiURL, true);
-
-  xhr.send('file=C:\Users\John\Hackathon\Hackathon\my-face.jpg&apikey=31073e76-4d1f-4615-9375-694ebfad9db7');
-}
-
-var reqListener = function (xhr) {
-  alert(xhr.responseXML);
+  xhr.open("GET", apiURL, false);
+  
+  xhr.send();
+  
+  var xhrDoc = xhr.response;
+  
+  console.log(xhrDoc);
 }
 
 sendRequest();
